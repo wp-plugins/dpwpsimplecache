b3ab69c99dec227508941e0ebc1197c3552f2add
danpai <danpai@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-02 09:51:22 +0000

    Add tag 0.3.1
    git-svn-id: https://plugins.svn.wordpress.org/dpwpsimplecache/tags/0.3.1@483305 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ dpwpcacheclass.php
@@ -0,0 +1,87 @@
+<?php
+
+class DP_Cache {
+        
+    function set($key, $data) {
+    	
+        $_SESSION["#$@" . $_SERVER['SERVER_NAME'] . $key] = base64_encode(serialize($data));
+    }
+
+    
+    function get($key) {
+    	
+        return unserialize(base64_decode($_SESSION["#$@" . $_SERVER['SERVER_NAME'] . $key]));
+    }
+
+        
+    function delete($key) {
+    	
+        unset($_SESSION["#$@" . $_SERVER['SERVER_NAME'] . $key]);
+    }
+    
+    function get_statistics(){
+    	
+    	$count = 0;
+    	foreach ($_SESSION as $key => $value){
+    		if(strpos($key,"#$@" . $_SERVER['SERVER_NAME']) !== false){
+    			$count++;
+    		}
+    	}
+    	return $count;
+    }
+    
+    function get_sessions_number(){
+    	global $wpdb;
+    	global $USE_DB_SESSION_MANAGER;
+    	if($USE_DB_SESSION_MANAGER){
+    		$tablename = $wpdb->prefix . "sessions";
+    		return $wpdb->get_var("SELECT COUNT(*) FROM " . $tablename);
+    	}
+    	else{
+    		$this->get_statistics();
+    	}
+    }
+    
+    function get_all_values(){   	
+    	$cached_element = array();
+    	foreach ($_SESSION as $key => $value){
+    		if(strpos($key,"#$@" . $_SERVER['SERVER_NAME']) !== false){
+    			$cached_element[str_replace("#$@" . $_SERVER['SERVER_NAME'], "", $key)] = $value; 
+    		}
+    	}
+    	return $cached_element;
+    }
+    
+    function contais($key){
+    	
+    	if(count($_SESSION)<1)
+    		return false;
+    	return array_key_exists("#$@" . $_SERVER['SERVER_NAME'] . $key, $_SESSION);
+    }
+    
+    function flush($all=false){
+    	global $wpdb;
+    	global $USE_DB_SESSION_MANAGER;
+    	if(!$all || !$USE_DB_SESSION_MANAGER){  	
+    		foreach ($_SESSION as $key => $value){
+    			if(strpos($key,"#$@" . $_SERVER['SERVER_NAME']) !== false){
+    				unset($_SESSION[$key]);
+    			}
+    		}
+    	}
+    	else {
+    		$date = new DateTime();
+			$tablename = $wpdb->prefix . "sessions";
+			$query = "truncate table " . $tablename;
+			$wpdb->query($query);
+    	}
+    }
+    
+    function inspect(){
+    	print_r($_SESSION);
+    }
+
+}
+
+global $dpcache;
+$dpcache = new DP_Cache();
